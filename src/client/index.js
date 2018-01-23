34a2eeda9d28f693adcf24eccc63da0169885f77
Rick van der Linden <rickvdl@me.com>
2018-01-23 23:13:19 +0100

    Made a lot of improvements to the argument parsing logic and added the option to supply query parameters such as filter or query.

--- src/client/index.js
+++ src/client/index.js
@@ -6,6 +6,7 @@ export default class Client {
     this.version = version;
     this.token = token;
     this.format = 'json';
+    this.hostname = 'moneybird.com';
   }
 
   get (...args) {
@@ -30,19 +31,91 @@ export default class Client {
 
   request (...args) {
     const method = String(args[0]).toUpperCase();
+    const path = '/api/' + this.version + '/' + this.administrationId + Client.requestEndpoint(method, args) + '.' + this.format + Client.requestQueryString(method, args);
+    console.log(path);
     return https.request({
-      'hostname': 'moneybird.com',
+      'hostname': this.hostname,
       'headers': {
         'Authorization': 'Bearer ' + this.token
       },
-      'path': '/api/' + this.version + '/' + this.administrationId + '/' + (Client.requestHasBody(method) ? args.slice(1, -1) : args.slice(1)).join('/') + '.' + this.format,
+      'path': path,
       'port': 443,
       'method': method,
-      'body': (Client.requestHasBody(method) ? args.slice(-1)[0] : null)
+      'body': Client.requestBody(method, args)
     });
   }
 
   static requestHasBody (method) {
     return method === 'POST' || method === 'PATCH';
+  }
+
+  // Checks wether the request contains a query object
+  static requestHasQuery(method, args) {
+    if (args.length <= 2) { return false; }
+    const objectCount = args.reduce((prev, cur) => {
+        return cur instanceof Object ? prev + 1 : prev;
+    }, 0);
+
+    if (Client.requestHasBody(method) && objectCount >= 2) {
+      return true;
+    } else if (!Client.requestHasBody(method) && objectCount >= 1) {
+      return true;
+    }
+
+    return false;
+  }
+
+  // Checks wether the request contains a body object
+  static requestBody(method, args) {
+    if (Client.requestHasBody(method)) {
+      return Client.requestHasQuery(method, args) ? args[args.length - 2] : args[args.length - 1];
+    }
+    return null;
+  }
+
+  // Returns the query string when the request contains a query object, else an empty string
+  static requestQueryString(method, args) {
+    if (!Client.requestHasQuery(method, args)) {
+      return '';
+    }
+
+    let queryParams = args[args.length - 1];
+    if (!queryParams instanceof Object) {
+      return '';
+    }
+
+    let queryString = '';
+    for (let queryParamKey in queryParams) {
+      let queryParamValue = queryParams[queryParamKey];
+      queryString = queryString + (queryString == '' ? '?' : '&');
+      queryString = queryString + encodeURIComponent(queryParamKey) + '=' + encodeURIComponent(queryParamValue);
+    }
+
+    return queryString;
+  }
+
+  // Returns the requested endpoint, joins all the parts together and makes sure the slashes match up
+  static requestEndpoint(method, args) {
+    let endIndex = args.length;
+
+    if (Client.requestHasBody(method, args)) {
+      endIndex--;
+    }
+
+    if (Client.requestHasQuery(method, args)) {
+      endIndex--;
+    }
+
+    let parts = args.slice(1, endIndex).filter((item) => {
+      return item !== Object(item);
+    });
+
+    if (parts.length > 1) {
+      return '/' + parts.join('/');
+    } else if (parts.length == 1) {
+      return '/' + parts[0];
+    }
+
+    return '';
   }
 }
